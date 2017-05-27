ac83030eb19275c8e5a5ff85c737c7b21fa2e070
akos@pingdom.com <akos@pingdom.com>
2017-05-27 16:55:19 +0200

    smoke added to contrabirds

--- src/game/contraBird.ts
+++ src/game/contraBird.ts
@@ -3,7 +3,7 @@ import { TICKER, GAME_WIDTH, GAME_HEIGHT } from "../app";
 import Utils from "../utils/utils";
 import * as particles from "pixi-particles";
 import { settings as EXPLOTION_SETTINGS } from "./contraBirdExplotion";
-
+import { settings as SMOKE_SETTINGS } from "./contrabirdSmoke";
 
 export var Events = {
 	DESTROYED: "contraBirdDestroyed"
@@ -25,6 +25,8 @@ export class ContraBird extends PIXI.Container {
 	private _body: PIXI.Sprite;
 	private _explotion: particles.Emitter;
 	private _explotionLifeTime = 0;
+	private _smoke: particles.Emitter;
+	private _smokeCenter: PIXI.Container;
 
 	constructor() {
 		super();
@@ -42,17 +44,26 @@ export class ContraBird extends PIXI.Container {
 	}
 
 	private _create = () => {
+		this._smokeCenter = new PIXI.Container();
 		switch (this._type) {
 			case "bird1":
 				this._body = new PIXI.Sprite(PIXI.Texture.fromImage("./assets/contraBirds/contra1.png"));
+				this._smokeCenter.x = 0;
+				this._smokeCenter.y = -10;
 				break;
 			case "bird2":
 				this._body = new PIXI.Sprite(PIXI.Texture.fromImage("./assets/contraBirds/contra2.png"));
+				this._smokeCenter.x = 0;
+				this._smokeCenter.y = 0;
 				break;
 			case "bird3":
 				this._body = new PIXI.Sprite(PIXI.Texture.fromImage("./assets/contraBirds/contra3.png"));
+				this._smokeCenter.x = 0;
+				this._smokeCenter.y = -30;
 				break;
 			case "bird4":
+				this._smokeCenter.x = 0;
+				this._smokeCenter.y = -10;
 				this._body = new PIXI.Sprite(PIXI.Texture.fromImage("./assets/contraBirds/contra4.png"));
 				break;
 			default:
@@ -61,12 +72,16 @@ export class ContraBird extends PIXI.Container {
 		}
 		this.x = GAME_WIDTH + this._BIRD_BUFFER;
 		this.y = Utils.getRandomNumberBetween(0, GAME_HEIGHT - this._BIRD_BUFFER);
+		TICKER.add(this._smoking);
+		this.addChild(this._smokeCenter);
 		this.addChild(this._body);
+		this._smoke = new particles.Emitter(this._smokeCenter, [PIXI.Texture.fromImage(this._EXPLOTION_PARTICLE_IMAGE)], SMOKE_SETTINGS);
 	};
 	private _move = () => {
 		this.x -= this._speed;
 		if (this.x < - this._BIRD_BUFFER - this.width) {
 			TICKER.remove(this._move);
+			TICKER.remove(this._smoking);
 			this.parent.removeChild(this);
 			this.emit(Events.DESTROYED, this.id);
 
@@ -82,4 +97,9 @@ export class ContraBird extends PIXI.Container {
 		this._explotionLifeTime++;
 		this.alpha -= 0.2;
 	}
+
+	private _smoking = (delta: number) => {
+		this._smoke.update(delta);
+		this._smoke.updateOwnerPos
+	};
 }
---
+++ src/game/contrabirdSmoke.ts
@@ -0,0 +1,53 @@
+export var settings = {
+	"alpha": {
+		"start": 0.01,
+		"end": 0.02
+	},
+	"scale": {
+		"start": 0.3,
+		"end": 1,
+		"minimumScaleMultiplier": 1
+	},
+	"color": {
+		"start": "#85888d",
+		"end": "#100f0c"
+	},
+	"speed": {
+		"start": 100,
+		"end": 400,
+		"minimumSpeedMultiplier": 1.03
+	},
+	"acceleration": {
+		"x": 0,
+		"y": 0
+	},
+	"maxSpeed": 0,
+	"startRotation": {
+		"min": -5,
+		"max": 5
+	},
+	"noRotation": false,
+	"rotationSpeed": {
+		"min": 0,
+		"max": 0
+	},
+	"lifetime": {
+		"min": 0.5,
+		"max": 1
+	},
+	"blendMode": "normal",
+	"frequency": 0.001,
+	"emitterLifetime": -1,
+	"maxParticles": 300,
+	"pos": {
+		"x": 50,
+		"y": 100
+	},
+	"addAtBack": true,
+	"spawnType": "circle",
+	"spawnCircle": {
+		"x": 0,
+		"y": 0,
+		"r": 3
+	}
+};
